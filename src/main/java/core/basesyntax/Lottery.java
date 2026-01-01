f162b77d05065c590cd10ac8258aa946e432115b
Maksym Tiutiunnykov <maximdk1@gmail.com>
2026-01-01 04:06:37 +0200

    moving an instance of the Ball class to the getRandomBall() method in the Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final int MAXIMUM_BALL_NUMBER = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private Ball ball = new Ball();
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         ball.setRandomColor(colorSupplier.getRandomColor());
         ball.setRandomNumber(new Random().nextInt(MAXIMUM_BALL_NUMBER));
         return ball;
