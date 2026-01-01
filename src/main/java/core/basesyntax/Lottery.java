e6682262052911465d54d0091d98fd4e5b131103
Maksym Tiutiunnykov <maximdk1@gmail.com>
2026-01-01 20:04:57 +0200

    In the Lottery class, a colorSupplier field was made final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAXIMUM_BALL_NUMBER = 101;
     private final Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
